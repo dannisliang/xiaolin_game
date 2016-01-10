93c90ba701a8a2b88efb2e60c3afa40af98824c2
home <798102394@qq.com>
2016-01-10 20:04:21 +0800

    join

--- Application/Home/Model/UserRoomModel.class.php
+++ Application/Home/Model/UserRoomModel.class.php
@@ -4,9 +4,38 @@ use Think\Model;
 
 class UserRoomModel extends CommModel {
 	
-	public function room_users($room_id){
+	public function room_users($room_id, $me = false){
 		$list = $this->where("room_id = '{$room_id}'")->select();
+		if($me){
+			foreach($list as &$low){
+				$low['me'] = false;
+				if($low['user_id']==UID){
+					$low['me'] = true;
+				}
+			}
+		}
 		return $list;
 	}
-
+	
+	public function into_room($user_id, $room_id ,$pos=0){
+		$in = $this->user_room($user_id);
+		if($in)return false;
+		$map['pos'] = $pos;
+		$map['ready'] = 0;
+		$map['user_id'] = $user_id;
+		$map['room_id'] = $room_id;
+		return $this->add($map);
+	}
+	
+	
+	public function user_room($user_id){
+		$rs = $this->where(" user_id = '{$user_id}'")->find();
+		return $rs;
+	}
+	
+	public function out_room($user_id){
+		return $this->where(" user_id = '{$user_id}'")->delete();
+	}
+	
+	
 }
