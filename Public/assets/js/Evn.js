e3bb26cb8cf2086f1ff583c8fc6d2cb29e5bbed1
home <798102394@qq.com>
2016-01-17 20:41:25 +0800

    click

--- Public/assets/js/Evn.js
+++ Public/assets/js/Evn.js
@@ -7,7 +7,7 @@
      var joining = false;
      
      //显示输入框
-    $('.js-inputroom').on("touchstart",function(){
+    $('.js-inputroom').on("touchstart  click",function(){
         $(this).animate({width: '0',opacity:'0'}, "slow");
         $('.inputr').animate({right: '15%'}, "slow");
         $('.arrow').animate({right: '-22%'}, "slow");
@@ -15,34 +15,34 @@
     
     
     //创建房间
-    $('.new').bind("touchstart",function(){
+    $('.new').bind("touchstart click",function(){
         Api.send('{"type":"login","method":"new","client_name":"'+window.MY_NAME+'"}');
     })
     
-    $('.quick').bind("touchstart",function(){
+    $('.quick').bind("touchstart click",function(){
         Api.send('{"type":"login","method":"quick","client_name":"'+window.MY_NAME+'"}');
     })
     
     //加入房间
-    $('.arrow').on("touchstart",function(){
+    $('.arrow').on("touchstart click",function(){
         var val = $(".inputr").val();
         if(!val||val=='')return;
         Api.send('{"type":"login","method":"join","client_name":"'+window.MY_NAME+'","room_id":"'+val+'"}');
     })
     
     //准备好了
-    $('.pk').bind("touchstart",function(){
+    $('.pk').bind("touchstart click",function(){
         Game.ready();
     })
     
     //触碰事件
-    $(".mygame").on('touchstart','.hidemyblock',function(){
+    $(".mygame").on('touchstart click','.hidemyblock',function(){
         var $this = $(this);
         if($this.hasClass("myblock"))return;
         Game.touch($this.attr("index"));
     })
     
-    $(".back").on('touchstart',function(){
+    $(".back").on('touchstart click',function(){
         
         Game.out();
     });
